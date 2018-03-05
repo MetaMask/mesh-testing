80858fa0a66218af8606862f7318290972997132
kumavis <aaron@kumavis.me>
2018-03-05 12:23:31 -0800

    build

--- bundle.js
+++ bundle.js
@@ -8618,7 +8618,7 @@ async function connectKitsunet(peerInfo, conn) {
 function pingKitsunetPeerWithTimeout(peer) {
   return Promise.race([
     timeout(5 * sec, 'timeout'),
-    pingKitsunetPeer(peer.rpc),
+    pingKitsunetPeer(peer),
   ])
 }
 
