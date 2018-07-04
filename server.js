e1e650b6981fece7774cb23bbd4c35b521ec2049
kumavis <aaron@kumavis.me>
2018-07-04 16:01:44 -0700

    server - overwrite state to remove orphaned nodes

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ setInterval(() => {
       delete networkState.clients[clientId]
     }
   })
-  networkStore.updateState(networkState)
+  networkStore.setState(networkState)
 }, 10 * sec)
 
 function disconnectClient(clientId) {
@@ -124,7 +124,7 @@ function disconnectClient(clientId) {
   // update network state
   const networkState = networkStore.getState()
   delete networkState.clients[clientId]
-  networkStore.updateState(networkState)
+  networkStore.setState(networkState)
   // report current connected count
   console.log(`${clients.length} peers connected`)
 }
@@ -151,7 +151,7 @@ async function handleClient(stream, req) {
       // update network state
       const networkState = networkStore.getState()
       networkState.clients[peerId] = { peers: [] }
-      networkStore.updateState(networkState)
+      networkStore.setState(networkState)
     },
     submitNetworkState: (peers) => {
       const peerId = client.peerId
@@ -159,7 +159,7 @@ async function handleClient(stream, req) {
       // update network state
       const networkState = networkStore.getState()
       networkState.clients[peerId] = { peers }
-      networkStore.updateState(networkState)
+      networkStore.setState(networkState)
     },
     disconnect: () => {
       console.log(`client "${client.peerId}" sent disconnect request`)
