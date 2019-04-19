1d899c120299759fdfbcf0f93f7354260d4de528
kumavis <aaron@kumavis.me>
2019-04-19 15:15:24 +0800

    admin - start rewrite as react

---
+++ src/admin/app/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+// import './App.css'
+// import Nav from './views/nav'
+
+// const routes = ['packages', 'modules']
+
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      // mode: routes[0],
+      // bundle: 'background',
+    }
+  }
+
+  // selectMode (target) {
+  //   this.setState(state => ({ mode: target }))
+  // }
+
+  // selectBundle (target) {
+  //   this.setState(state => ({ bundle: target }))
+  // }
+
+  render () {
+    // const bundleData = bundles[this.state.bundle]
+    return (
+      <div className="App">
+        hello
+        {/* <Nav
+          routes={bundleNames}
+          activeRoute={this.state.bundle}
+          onNavigate={(target) => this.selectBundle(target)}
+          />
+        <Nav
+          routes={routes}
+          activeRoute={this.state.mode}
+          onNavigate={(target) => this.selectMode(target)}
+          />
+        <DepGraph
+          bundleData={bundleData}
+          mode={this.state.mode}
+          />  */}
+      </div>
+    )
+  }
+}
+
+export default App
